7a319be182fc437e5dbfbed51e69d16365764461
patrickcorcoran10 <patrickcorcoran10@gmail.com>
2021-07-01 21:32:31 -0500

    car model added

--- models/Car.js
+++ models/Car.js
@@ -2,3 +2,27 @@ const { Model, DataTypes } = require("sequelize");
 const sequelzie = require("../config/connection");
 
 class Car extends Model {}
+
+Car.init(
+    {
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        id_Manufacturers: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "manufacturers",
+                key: "id",
+                unique: false
+            }
+        }
+    }, 
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "car"
+    }
+)
